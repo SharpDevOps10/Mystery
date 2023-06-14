0cd88abc9da9513137dd0f39545ccf749fe1a86c
SharpDevOps10 <td090833@gmail.com>
2023-06-14 20:45:19 +0300

    Implement connection between models

--- server/models/models.js
+++ server/models/models.js
@@ -47,3 +47,48 @@ const DeviceInfo = sequelize.define('deviceInfo', {
   title: { type: DataTypes.STRING, allowNull: false },
   description: { type: DataTypes.STRING, allowNull: false },
 });
+
+
+const TypeBrand = sequelize.define('typeBrand', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
+
+
+User.hasOne(Basket);
+Basket.belongsTo(User);
+
+User.hasMany(Rating);
+Rating.belongsTo(User);
+
+Basket.hasMany(BasketDevice);
+BasketDevice.belongsTo(Basket);
+
+Type.hasMany(Device);
+Device.belongsTo(Type);
+
+Brand.hasMany(Device);
+Device.belongsTo(Brand);
+
+Device.hasMany(Rating);
+Rating.belongsTo(Device);
+
+Device.hasMany(BasketDevice);
+BasketDevice.belongsTo(Device);
+
+Device.hasMany(DeviceInfo);
+DeviceInfo.belongsTo(Device);
+
+Type.belongsToMany(Brand, { through: TypeBrand });
+Brand.belongsToMany(Type, { through: TypeBrand });
+
+module.exports = {
+  User,
+  Basket,
+  BasketDevice,
+  Device,
+  DeviceInfo,
+  Type,
+  Brand,
+  Rating,
+  TypeBrand
+};
