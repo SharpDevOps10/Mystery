086b325b55b1c370fd002e2f6eb56b524a0c227f
SharpDevOps10 <td090833@gmail.com>
2023-06-15 22:25:43 +0300

    Implement method that performs filtering and pagination of the list of devices

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -8,8 +8,8 @@ class DeviceController {
     try {
       const { name, price, brandId, typeId, info } = req.body;
       const { img } = req.files;
-      const fileName = uuid.v4() + '.jpg';
-      img.mv(path.resolve(__dirname, '..', 'static', fileName));
+      const fileName = uuid.v4() + ".jpg";
+      await img.mv(path.resolve(__dirname, '..', 'static', fileName));
       const device = await Device.create({
         name, price, brandId, typeId, img: fileName
       });
@@ -21,6 +21,24 @@ class DeviceController {
   }
 
   async getAll(req, res) {
+    let { brandId, typeId, limit, page} = req.body;
+    page = page || 1;
+    limit = limit || 9;
+    let offset = page * limit - limit;
+    let devices;
+    if (!brandId && !typeId) {
+      devices = await Device.findAndCountAll({limit, offset});
+    }
+    if (brandId && !typeId) {
+      devices = await Device.findAndCountAll({ where: {brandId}, limit, offset});
+    }
+    if (!brandId && typeId) {
+      devices = await Device.findAndCountAll({ where: {typeId}, limit, offset});
+    }
+    if (brandId && typeId) {
+      devices = await Device.findAndCountAll({ where: {typeId, brandId}, limit, offset});
+    }
+    return res.json(devices);
 
   }
 
--- server/index.js
+++ server/index.js
@@ -7,10 +7,12 @@ const cors = require('cors');
 const router = require('./routes/index');
 const fileUpload = require('express-fileupload');
 const errorHandler = require('./middlewares/errorHandlingMiddleware');
-const PORT = process.env.PORT;
+const PORT = 5000;
+const path = require('node:path');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')));
 app.use(fileUpload({}));
 app.use('/api', router);
 app.use(errorHandler);
@@ -18,7 +20,7 @@ const start = async () => {
   try {
     await sequelize.authenticate();
     await sequelize.sync();
-    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+    app.listen(5000, () => console.log(`Server started on port ${PORT}`));
   } catch (e) {
     console.log(e);
   }
