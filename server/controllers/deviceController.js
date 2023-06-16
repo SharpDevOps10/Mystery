2a4001348ebf7ae04ceee469afdb2236cc750f4f
SharpDevOps10 <td090833@gmail.com>
2023-06-16 16:25:37 +0300

    Implement handling `information`

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,18 +1,28 @@
 'use strict';
 const uuid = require('uuid');
 const path = require('node:path');
-const { Device } = require('../models/models');
+const { Device, DeviceInfo } = require('../models/models');
 const apiError = require('../errors/apiError');
 class DeviceController {
   async create(req, res, next) {
     try {
-      const { name, price, brandId, typeId, info } = req.body;
+      let { name, price, brandId, typeId, info } = req.body;
       const { img } = req.files;
       const fileName = uuid.v4() + '.jpg';
       await img.mv(path.resolve(__dirname, '..', 'static', fileName));
       const device = await Device.create({
         name, price, brandId, typeId, img: fileName
       });
+      if (info) {
+        info = JSON.parse(info);
+        info.forEach((i) => {
+          DeviceInfo.create({
+            title: i.title,
+            description: i.description,
+            deviceId: device.id,
+          });
+        });
+      }
 
       return res.json(device);
     } catch (error) {
