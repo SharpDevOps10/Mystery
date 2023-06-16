510c9d2ad24b93d55be5958121a11311c2fcde67
SharpDevOps10 <td090833@gmail.com>
2023-06-16 18:49:02 +0300

    Add middleware that is used to validate and authenticate the access token passed in the request header

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -4,10 +4,12 @@ const { Router } = require('express');
 const router = Router();
 const userController = require('../controllers/userController.js');
 
+const authMiddleware = require('../middlewares/authMiddleware');
+
 router.post('/registration', userController.registration);
 
 router.post('/login', userController.login);
 
-router.get('/authorization', userController.checkAuthorization);
+router.get('/authorization', authMiddleware ,userController.checkAuthorization);
 
 module.exports = router;
