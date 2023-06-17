357768c89c23aba5379c07c1c6bc4ece7556bd34
SharpDevOps10 <td090833@gmail.com>
2023-06-17 21:56:39 +0300

    Implement logic to switch into `sign in` or `auth` buttons

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,10 +2,11 @@
 
 import React, {useContext} from 'react';
 import {Context} from "../index";
-import {Container, Nav, Navbar, NavLink} from "react-bootstrap";
+import {Button, Container, Nav, Navbar, NavLink} from "react-bootstrap";
 import {SHOP_ROUTE} from "../utils/constants";
+import {observer} from "mobx-react-lite";
 
-const NavBar = () => {
+const NavBar = observer(() => {
   const {user} = useContext(Context);
 
   return (
@@ -14,16 +15,21 @@ const NavBar = () => {
       <Navbar bg="primary" data-bs-theme="dark">
         <Container>
           <NavLink style={{color: 'white'}} to={SHOP_ROUTE}>Adolf`s House</NavLink>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
+          {user.isAuth ?
+            <Nav className="ml-auto" style={{color: 'white'}}>
+              <Button variant={'outline-light'}>Admin</Button>
+              <Button variant={'outline-light'} className="ms-lg-2">Sign in</Button>
+            </Nav>
+            :
+            <Nav className="ml-auto" style={{color: 'white'}}>
+              <Button variant={'outline-light'} onClick={() => user.setIsAuth(true)}>Authorization</Button>
+            </Nav>
+          }
         </Container>
       </Navbar>
 
     </>
   );
-};
+});
 
 export default NavBar;
