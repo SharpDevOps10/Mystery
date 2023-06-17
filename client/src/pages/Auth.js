bd04041c3af3a8b8dcaede8a7addda99adc3c6f3
SharpDevOps10 <td090833@gmail.com>
2023-06-17 22:14:32 +0300

    Add buttons for login page

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,13 +1,31 @@
 'use strict';
 
 import React from 'react';
+import {Card, Container, Form} from "react-bootstrap";
 
 const Auth = () => {
   return (
-    <div>
-      AUTH
+    <Container
+      className="d-flex justify-content-center align-items-center"
+      style={{height: window.innerHeight - 54}}
+    >
+      <Card style={{width: 600}} className="p-5">
+        <h2 className="m-auto">
+          Authorization
+        </h2>
+        <Form className="d-flex flex-column">
+          <Form.Control
+            className="mt-2"
+            placeholder="Enter your email"
+          />
+          <Form.Control
+            className="mt-2"
+            placeholder="Enter your password"
+          />
+        </Form>
+      </Card>
 
-    </div>
+    </Container>
   );
 };
 
