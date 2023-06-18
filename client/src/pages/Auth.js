bc0aed56ce5203ebf6e74b770afc9ccac5265a96
SharpDevOps10 <td090833@gmail.com>
2023-06-18 20:09:14 +0300

    Update link: * can switch between `Registration` or `Authorization`

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -2,17 +2,26 @@
 
 import React from 'react';
 import {Button, Card, Container, Form, NavLink, Row} from "react-bootstrap";
-import {REGISTRATION_ROUTE} from "../utils/constants";
+import {LOGIN_ROUTE, REGISTRATION_ROUTE} from "../utils/constants";
+import {useLocation} from "react-router-dom";
 
 const Auth = () => {
+
+  const location = useLocation();
+  const isLogin = location.pathname === LOGIN_ROUTE;
+
+
   return (
     <Container
       className="d-flex justify-content-center align-items-center"
-      style={{ height: window.innerHeight - 54 }}
+      style={{height: window.innerHeight - 54}}
     >
-      <Card style={{ width: 600 }} className="p-5">
+      <Card style={{width: 600}} className="p-5">
         <h2 className="m-auto">
-          Authorization
+          { isLogin
+            ? "Authorization"
+            : "Registration"
+          }
         </h2>
         <Form className="d-flex flex-column">
           <Form.Control
@@ -25,11 +34,19 @@ const Auth = () => {
           />
         </Form>
         <Row className="d-flex justify-content-between mt-3 pl-3">
-          <div style={{display: 'inline-flex'}}>
-            No account?<NavLink to={REGISTRATION_ROUTE}> Please, authorize!</NavLink>
+          { isLogin ?
+            <div style={{display: 'inline-flex'}}>
+            No account?<NavLink to={REGISTRATION_ROUTE}> Please, Register!</NavLink>
           </div>
+            : <div style={{display: 'inline-flex'}}>
+              Have account yet?<NavLink to={LOGIN_ROUTE}> Please, Sign In!</NavLink>
+            </div>
+          }
           <Button variant={"outline-success"}>
-            Sign In
+            { isLogin ? "Sign In"
+              : "Registration"
+
+            }
           </Button>
         </Row>
       </Card>
