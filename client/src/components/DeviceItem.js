8d901d47b9f89e21f6d060ce75f1de3ce3f35c64
SharpDevOps10 <td090833@gmail.com>
2023-06-19 12:11:44 +0300

    Add clickable goods with `id`

--- client/src/components/DeviceItem.js
+++ client/src/components/DeviceItem.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {Card, Col, Image} from "react-bootstrap";
 import star from '../assets/black-star-transparent-3.png';
+import {useHistory} from "react-router-dom";
+import {DEVICE_ROUTE} from "../utils/constants";
 
 const DeviceItem = ({device}) => {
+  const history = useHistory();
   return (
-    <Col md={3}>
+    <Col md={3} className="mt-3" onClick={() => history.push(DEVICE_ROUTE + '/' + device.id)}>
       <Card style={{width: 150, cursor: 'pointer'}} border='light'>
         <Image src={device.img} width={150} height={150}></Image>
         <div className="text-black-50 mt-1 d-flex justify-content-between align-self-center">
