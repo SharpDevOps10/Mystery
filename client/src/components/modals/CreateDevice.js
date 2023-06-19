4e3aeef0b2cb538bc6333324ba8272e95bd5df60
SharpDevOps10 <td090833@gmail.com>
2023-06-19 18:58:14 +0300

    Implement feature: * After clicking the button, modal windows will appear

--- client/src/components/modals/CreateDevice.js
+++ client/src/components/modals/CreateDevice.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import {Button, Form, Modal} from "react-bootstrap";
 
-const CreateDevice = () => {
+const CreateDevice = ({show, onHide}) => {
   return (
-    <div>
-      
-    </div>
+    <Modal
+      show={show}
+      size="lg"
+      onHide={onHide}
+
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Upload new type
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Control
+            placeholder={"Upload the name of type"}
+          />
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant={"outline-danger"} onClick={onHide}>Close</Button>
+        <Button variant={"outline-success"} onClick={onHide}>Upload</Button>
+      </Modal.Footer>
+    </Modal>
   );
 };
 
--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,20 +1,23 @@
 'use strict';
 
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Container, Modal} from "react-bootstrap";
 import CreateBrand from "../components/modals/CreateBrand";
 import CreateType from "../components/modals/CreateType";
 import CreateDevice from "../components/modals/CreateDevice";
 
 const Admin = () => {
+  const {brandVisible, setBrandVisible} = useState(false);
+  const {typeVisible, setTypeVisible} = useState(false);
+  const {deviceVisible, setDeviceVisible} = useState(false);
   return (
     <Container className="d-flex flex-column">
-      <Button variant={"outline-dark"} className="mt-4 p-2">Add type</Button>
-      <Button variant={"outline-dark"} className="mt-4 p-2">Add brand</Button>
-      <Button variant={"outline-dark"} className="mt-4 p-2">Add device</Button>
-      <CreateBrand/>
-      <CreateType/>
-      <CreateDevice></CreateDevice>
+      <Button variant={"outline-dark"} className="mt-4 p-2" onClick={() => setTypeVisible(true)}>Add type</Button>
+      <Button variant={"outline-dark"} className="mt-4 p-2" onClick={() => setBrandVisible(true)}>Add brand</Button>
+      <Button variant={"outline-dark"} className="mt-4 p-2" onClick={() => setDeviceVisible(true)}>Add device</Button>
+      <CreateBrand show={brandVisible} onHide={() => setBrandVisible(false)}/>
+      <CreateType show={typeVisible} onHide={() => setTypeVisible(false)}/>
+      <CreateDevice show={deviceVisible} onHide={() => setDeviceVisible(false)}/>
     </Container>
   );
 };
