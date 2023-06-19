15f22f7412b3b88d0dbefafb0c5e6855815aff38
SharpDevOps10 <td090833@gmail.com>
2023-06-19 11:52:06 +0300

    Add `star` image near the card of device

--- client/src/components/DeviceItem.js
+++ client/src/components/DeviceItem.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Card, Col, Image} from "react-bootstrap";
+import star from '../assets/black-star-transparent-3.png';
 
 const DeviceItem = ({device}) => {
   return (
     <Col md={3}>
       <Card style={{width: 150, cursor: 'pointer'}} border='light'>
         <Image src={device.img} width={150} height={150}></Image>
-        <div>Samsung</div>
+        <div className="d-flex justify-content-between align-self-center">Samsung</div>
+        <Image src={star}></Image>
 
 
       </Card>
