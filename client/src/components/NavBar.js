92f882b6fe656db51a002ad06daf0a915080b914
SharpDevOps10 <td090833@gmail.com>
2023-06-20 19:39:15 +0300

    Change `navBar` according to authorization status

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,25 +1,30 @@
 'use strict';
 
-import React, {useContext} from 'react';
-import {Context} from "../index";
-import {Button, Container, Nav, Navbar, NavLink} from "react-bootstrap";
-import {ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE} from "../utils/constants";
-import {observer} from "mobx-react-lite";
-import {useHistory} from "react-router-dom";
-
+import React, { useContext } from 'react';
+import { Context } from '../index';
+import { Button, Container, Nav, Navbar, NavLink } from 'react-bootstrap';
+import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from '../utils/constants';
+import { observer } from 'mobx-react-lite';
+import { useHistory } from 'react-router-dom';
 
 const NavBar = observer(() => {
-  const {user} = useContext(Context);
-  const {history} = useHistory();
+  const { user } = useContext(Context);
+  const { history } = useHistory();
+  const logOut = () => {
+    user.setUser(true);
+    user.setIsAuth(true);
+  };
 
   return (
     <>
-      <br/>
+      <br />
       <Navbar bg="primary" data-bs-theme="dark">
         <Container>
-          <NavLink style={{color: 'white'}} to={SHOP_ROUTE}>Adolf`s House</NavLink>
-          {user.isAuth ?
-            <Nav className="ml-auto" style={{color: 'white'}}>
+          <NavLink style={{ color: 'white' }} to={SHOP_ROUTE}>
+            Adolf`s House
+          </NavLink>
+          {user.isAuth ? (
+            <Nav className="ml-auto" style={{ color: 'white' }}>
               <Button
                 variant={'outline-light'}
                 onClick={() => history.push(ADMIN_ROUTE)}
@@ -28,20 +33,24 @@ const NavBar = observer(() => {
               </Button>
               <Button
                 variant={'outline-light'}
-                onClick={() => history.push(LOGIN_ROUTE)}
+                onClick={() => logOut()}
                 className="ms-lg-2"
               >
                 Sign out
               </Button>
             </Nav>
-            :
-            <Nav className="ml-auto" style={{color: 'white'}}>
-              <Button variant={'outline-light'} onClick={() => user.setIsAuth(true)}>Authorization</Button>
+          ) : (
+            <Nav className="ml-auto" style={{ color: 'white' }}>
+              <Button
+                variant={'outline-light'}
+                onClick={() => history.push(LOGIN_ROUTE)}
+              >
+                Authorization
+              </Button>
             </Nav>
-          }
+          )}
         </Container>
       </Navbar>
-
     </>
   );
 });
