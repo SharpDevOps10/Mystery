73e6375135a266004d911212a582258494394b93
SharpDevOps10 <td090833@gmail.com>
2023-06-21 17:15:32 +0300

    The device loads after opening the page

--- client/src/pages/Device.js
+++ client/src/pages/Device.js
@@ -1,21 +1,21 @@
 'use strict';
 
-import React from 'react';
-import {Col, Container} from "react-bootstrap";
+import React, { useEffect, useState } from 'react';
+import { Col, Container } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
+import { fetchOneDevice } from '../http/deviceAPI';
 
 const Device = () => {
+  const [device, setDevice] = useState({ info: [] });
+  const { id } = useParams();
+  useEffect(() => {
+    fetchOneDevice(id).then((data) => setDevice(data));
+  }, []);
   return (
     <Container>
-      <Col md={4}>
-
-      </Col>
-      <Col md={4}>
-
-      </Col>
-      <Col md={4}>
-
-      </Col>
-
+      <Col md={4}></Col>
+      <Col md={4}></Col>
+      <Col md={4}></Col>
     </Container>
   );
 };
