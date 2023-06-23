1c49aea75868ee8d95060429c832ba3f4bc1d524
SharpDevOps10 <td090833@gmail.com>
2023-06-23 17:07:35 +0300

    Implement function that adds `types`

--- client/src/components/modals/CreateType.js
+++ client/src/components/modals/CreateType.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import {Button, Form, Modal} from "react-bootstrap";
+import React, { useState } from 'react';
+import { Button, Form, Modal } from 'react-bootstrap';
+import { createType } from '../../http/deviceAPI';
 
-const CreateType = ({show, onHide}) => {
+const CreateType = ({ show, onHide }) => {
+  const [value, setValue] = useState('');
+  const addType = () => {
+    createType({ name: value }).then((data) => {
+      setValue('');
+      onHide();
+    });
+  };
   return (
-    <Modal
-      show={show}
-      size="lg"
-      onHide={onHide}
-
-      centered
-    >
+    <Modal show={show} size="lg" onHide={onHide} centered>
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
           Upload new type
@@ -18,13 +20,19 @@ const CreateType = ({show, onHide}) => {
       <Modal.Body>
         <Form>
           <Form.Control
-          placeholder={"Upload the name of type"}
+            value={value}
+            onChange={(event) => setValue(event.target.value)}
+            placeholder={'Upload the name of type'}
           />
         </Form>
       </Modal.Body>
       <Modal.Footer>
-        <Button variant={"outline-danger"} onClick={onHide}>Close</Button>
-        <Button variant={"outline-success"} onClick={onHide}>Upload</Button>
+        <Button variant={'outline-danger'} onClick={onHide}>
+          Close
+        </Button>
+        <Button variant={'outline-success'} onClick={addType}>
+          Upload
+        </Button>
       </Modal.Footer>
     </Modal>
   );
