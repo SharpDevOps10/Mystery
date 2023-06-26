0e8b67830333f9600b24cd574ca97b7f220ad9ee
SharpDevOps10 <td090833@gmail.com>
2023-06-26 22:02:03 +0300

    Add `basketRouter` to general routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,11 +6,13 @@ const deviceRouter = require('./deviceRouter.js');
 const userRouter = require('./userRouter.js');
 const brandRouter = require('./brandRouter.js');
 const typeRouter = require('./typeRouter.js');
+const basketRouter = require('./basketRouter');
 
 router.use('/user', userRouter);
 router.use('/type', typeRouter);
 router.use('/brand', brandRouter);
 router.use('/device', deviceRouter);
+router.use('/basket', basketRouter);
 
 module.exports = router;
 
