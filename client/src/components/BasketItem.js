7be36a44d0848601ca41d92b0ecd54a4242b3ab8
SharpDevOps10 <td090833@gmail.com>
2023-06-28 21:31:19 +0300

    Add information about individual device in list of devices in basket

---
+++ client/src/components/BasketItem.js
@@ -0,0 +1,31 @@
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import star from '../assets/black-star-transparent-3.png';
+
+const BasketDeviceItem = ({ device }) => {
+  return (
+    <Col md={3} className="mt-3">
+      <div
+        style={{ cursor: 'pointer', width: '100%', border: '1px solid light' }}
+        className="d-flex"
+      >
+        <Image
+          width={150}
+          height={150}
+          src={process.env.REACT_APP_API_URL + device.img}
+        />
+        <div className="text-black-50 mt-1 d-flex align-items-center">
+          <div>{device.brand}</div>
+          <div className="d-flex align-items-center">
+            <div>{device.rating}</div>
+            <Image src={star} width={15} height={15} />
+          </div>
+          <div>{device.name}</div>
+          <div>Amount: {device.basket.length}</div>
+        </div>
+      </div>
+    </Col>
+  );
+};
+
+export default BasketDeviceItem;
