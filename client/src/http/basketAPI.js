658af3092efd35a18d765e86e3d3be057802164e
SharpDevOps10 <td090833@gmail.com>
2023-06-28 20:56:42 +0300

    Add basketAPI.js for: * adding devices to basket * receiving list of devices in basket

---
+++ client/src/http/basketAPI.js
@@ -0,0 +1,12 @@
+'use strict';
+import { $authHost } from '.';
+
+export const addDeviceToBasket = async (id, userId) => {
+  const { data } = await $authHost.post('api/device/' + id, { id, userId });
+  return data;
+};
+
+export const fetchBasketDevices = async (userId) => {
+  const { data } = await $authHost.get('api/basket/' + userId);
+  return data;
+};
