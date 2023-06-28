616aa4d20861a5c812c6b7fbe855e094ba982d79
SharpDevOps10 <td090833@gmail.com>
2023-06-28 21:56:41 +0300

    Add component that binds data and functionality from the basket store to displaying list of devices in basket

--- client/src/pages/Basket.js
+++ client/src/pages/Basket.js
@@ -1,14 +1,27 @@
-'use strict';
+import { useContext, useEffect } from 'react';
+import { Context } from '../components/Providers';
+import { fetchBasketDevices } from '../http/basketApi';
+import { observer } from 'mobx-react-lite';
+import BasketDeviceItem from '../components/BasketItem';
+import Container from 'react-bootstrap/esm/Container';
+import { useParams } from 'react-router-dom';
 
-import React from 'react';
+const Basket = observer(() => {
+  const { basket } = useContext(Context);
+  const { id } = useParams();
 
-const Basket = () => {
+  useEffect(() => {
+    fetchBasketDevices(id).then((data) => {
+      basket.setDevices(data);
+    });
+  }, [basket, id]);
+
   return (
-    <div>
-      Basket
-
-    </div>
+    <Container>
+      {basket.devices.map((device) => (
+        <BasketDeviceItem key={device.id} device={device} />
+      ))}
+    </Container>
   );
-};
-
+});
 export default Basket;
